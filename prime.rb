31a2467a142784c3f18f90e55602fc6588b2b62a
EJLarcs <ejlarcombe@gmail.com>
2015-07-30 16:28:07 -0400

    prime testing

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(number)
+ for n in (2...number) # for the number in the range 2 to the number before inserted-number
+   if (number % n) == 0 #if 
+     return false 
+   end
+ end
+ true 
+end
